e1b44f7b67edd9a3f8f80daf86100c727a08e936
Javohir <161417608+Javohir-Khursanboyev@users.noreply.github.com>
2024-05-22 00:03:51 +0500

    Added AssetService

---
+++ src/CareConnect.Service/Services/Assets/IAssetService.cs
@@ -0,0 +1,10 @@
+﻿using CareConnect.Service.DTOs.Assets;
+
+namespace CareConnect.Service.Services.Assets;
+
+public interface IAssetService
+{
+    Task<AssetViewModel> UploadAsync(AssetCreateModel model);
+    Task<bool> DeleteAsync(long id);
+    Task<AssetViewModel> GetByIdAsync(long id);
+}
