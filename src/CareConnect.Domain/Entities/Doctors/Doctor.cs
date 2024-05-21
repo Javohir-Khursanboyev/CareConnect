f0b9f877a1c1204d79555c0dca8324241f035e2c
Komiljon <komiljonrahmatov03@gmail.com>
2024-05-21 19:37:32 +0500

    Added Hospital Entity

---
+++ src/CareConnect.Domain/Entities/Doctors/Doctor.cs
@@ -0,0 +1,9 @@
+﻿using CareConnect.Domain.Commons;
+
+namespace CareConnect.Domain.Entities.Doctors;
+
+public class Doctor : Auditable
+{
+    public string Specialty { get; set; }
+
+}
---
+++ src/CareConnect.Domain/Hospitals/Hospital.cs
@@ -0,0 +1,18 @@
+﻿using CareConnect.Domain.Commons;
+using CareConnect.Domain.Entities.Assets;
+using CareConnect.Domain.Entities.Users;
+
+namespace CareConnect.Domain.Hospitals;
+
+public class Hospital : Auditable
+{
+    public string Name { get; set; }
+    public string Address { get; set; }
+    public string Phone { get; set; }
+    public string Email { get; set; }
+    public string Description { get; set; }
+    public long AssetId { get; set; }
+
+    public User User { get; set; }
+    public Asset Asset { get; set; }
+}
