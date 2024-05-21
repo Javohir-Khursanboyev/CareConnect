2254cc35e61d89692ad6259dad110fcf5e40cb3a
Komiljon <komiljonrahmatov03@gmail.com>
2024-05-22 00:12:09 +0500

    Added IPermissionService

---
+++ src/CareConnect.Service/Services/Permissions/IPermissionService.cs
@@ -0,0 +1,13 @@
+﻿using CareConnect.Service.Configurations;
+using CareConnect.Service.DTOs.Permissions;
+
+namespace CareConnect.Service.Services.Permissions;
+
+public interface IPermissionService
+{
+    Task<PermissionViewModel> CreateAsync(PermissionCreateModel model);
+    Task<PermissionViewModel> UpdateAsync(long id, PermissionUpdateModel model);
+    Task<bool> DeleteAsync(long id);
+    Task<PermissionViewModel> GetByIdAsync(long id);
+    Task<IEnumerable<PermissionViewModel>> GetAllAsync(PaginationParams @params, Filter filter, string search = null);
+}
