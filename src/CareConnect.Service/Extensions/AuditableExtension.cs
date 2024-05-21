7f383f9d9334e0c034e86567a27ebc12d773d65f
Javohir <161417608+Javohir-Khursanboyev@users.noreply.github.com>
2024-05-21 21:40:55 +0500

    Added Auditable extension

---
+++ src/CareConnect.Service/Extensions/AuditableExtension.cs
@@ -0,0 +1,25 @@
+﻿using CareConnect.Domain.Commons;
+using CareConnect.Service.Helpers;
+
+namespace CareConnect.Service.Extensions;
+
+public static class AuditableExtension
+{
+    public static void Create(this Auditable auditable)
+    {
+        auditable.CreatedAt = DateTime.UtcNow;
+        auditable.CreatedByUserId = HttpContextHelper.UserId;
+    }
+
+    public static void Update(this Auditable auditable)
+    {
+        auditable.UpdatedAt = DateTime.UtcNow;
+        auditable.UpdatedByUserId = HttpContextHelper.UserId;
+    }
+
+    public static void Delete(this Auditable auditable)
+    {
+        auditable.DeletedAt = DateTime.UtcNow;
+        auditable.DeletedByUserId = HttpContextHelper.UserId;
+    }
+}
