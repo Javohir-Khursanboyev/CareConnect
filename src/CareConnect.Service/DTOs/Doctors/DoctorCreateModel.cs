e690d13daaaae3e50b485760befb90baf93b9dc6
Komiljon <komiljonrahmatov03@gmail.com>
2024-05-21 22:40:42 +0500

    Added Doctor DTOs

---
+++ src/CareConnect.Service/DTOs/Doctors/DoctorCreateModel.cs
@@ -0,0 +1,9 @@
+﻿namespace CareConnect.Service.DTOs.Doctors;
+
+public class DoctorCreateModel
+{
+    public string Specialty { get; set; }
+    public long DepartmentId { get; set; }
+    public long? PictureId { get; set; }
+    public long ResumeId { get; set; }
+}
---
+++ src/CareConnect.Service/DTOs/Doctors/DoctorViewModel.cs
@@ -0,0 +1,13 @@
+﻿using CareConnect.Service.DTOs.Assets;
+using CareConnect.Service.DTOs.Departments;
+
+namespace CareConnect.Service.DTOs.Doctors;
+
+public class DoctorViewModel
+{
+    public long Id { get; set; }
+    public string Specialty { get; set; }
+    public DepartmentViewModel Department { get; set; }
+    public AssetViewModel Picture { get; set; }
+    public AssetViewModel Resume { get; set; }
+}
