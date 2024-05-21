6c47fd3e60a7e50165699de2b6bf464db4ea6a40
Javohir <161417608+Javohir-Khursanboyev@users.noreply.github.com>
2024-05-21 22:38:30 +0500

    Added Program Register

--- src/CareConnect.WebApi/Program.cs
+++ src/CareConnect.WebApi/Program.cs
@@ -1,13 +1,43 @@
+using CareConnect.Data.DbContexts;
+using CareConnect.WebApi.Extensions;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(options =>
+    options.Conventions.Add(new RouteTokenTransformerConvention(new ConfigurationApiUrlName())));
+builder.Services.AddEndpointsApiExplorer();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+builder.Services.AddDbContext<AppDbContext>(
+    options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultDbConnection")));
+
+builder.Services.AddExceptionHandlers();
+builder.Services.AddProblemDetails();
+
+builder.Services.ConfigureSwagger();
+builder.Services.AddJwtService(builder.Configuration);
+//builder.Services.AddAutoMapper(typeof(MappingProfile));
+builder.Services.AddServices();
+builder.Services.AddValidators();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+app.AddInjectHelper();
+app.InjectEnvironmentItems();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var dbContext = services.GetRequiredService<AppDbContext>();
+    dbContext.Database.EnsureCreated();
+    dbContext.Database.Migrate();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -21,5 +51,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseExceptionHandler();
 
 app.Run();
