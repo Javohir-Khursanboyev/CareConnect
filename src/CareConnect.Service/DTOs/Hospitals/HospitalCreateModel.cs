d5677135d2de4bba56d4f2b4a5f7f05a8705c60f
Komiljon <komiljonrahmatov03@gmail.com>
2024-05-21 22:31:37 +0500

    Added Hospital DTOs

---
+++ src/CareConnect.Service/DTOs/Hospitals/HospitalCreateModel.cs
@@ -0,0 +1,11 @@
+﻿namespace CareConnect.Service.DTOs.Hospitals;
+
+public class HospitalCreateModel
+{
+    public string Name { get; set; }
+    public string Address { get; set; }
+    public string Phone { get; set; }
+    public string Email { get; set; }
+    public string Description { get; set; }
+    public long AssetId { get; set; }
+}
