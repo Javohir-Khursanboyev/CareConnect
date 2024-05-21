7a3d77ce83be7cc25aa511d28f4ae2f6fcc1e96f
Komiljon <komiljonrahmatov03@gmail.com>
2024-05-21 22:47:42 +0500

    Added DoctorComment DTOs

---
+++ src/CareConnect.Service/DTOs/DoctorComments/DoctorCommentCreateModel.cs
@@ -0,0 +1,9 @@
+﻿namespace CareConnect.Service.DTOs.DoctorComments;
+
+public class DoctorCommentCreateModel
+{
+    public long DoctorId { get; set; }
+    public long PatientId { get; set; }
+    public string Comment { get; set; }
+    public long? ParentId { get; set; }
+}
