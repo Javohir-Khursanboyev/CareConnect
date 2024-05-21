d0467602186b86150c040aaf68e222ab05cf6ce6
Komiljon <komiljonrahmatov03@gmail.com>
2024-05-21 22:09:07 +0500

    Added User DTOs

---
+++ src/CareConnect.Service/DTOs/Users/UserUpdateModel.cs
@@ -0,0 +1,13 @@
+﻿namespace CareConnect.Service.DTOs.Users;
+
+public class UserUpdateModel
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string Phone { get; set; }
+    public string Password { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public char Gender { get; set; }
+    public long RoleId { get; set; }
+}
---
+++ src/CareConnect.Service/DTOs/Users/UserViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace CareConnect.Service.DTOs.Users;
+
+public class UserViewModel
+{
+    public long Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string Phone { get; set; }
+    public string Password { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public char Gender { get; set; }
+    public long RoleId { get; set; }
+}
