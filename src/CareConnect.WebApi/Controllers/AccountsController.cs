06b3c39da3cde1e5a1c029b98c5063fc099ae5e1
Komiljon <komiljonrahmatov03@gmail.com>
2024-05-22 00:47:28 +0500

    Added Accounts Controller

---
+++ src/CareConnect.WebApi/Controllers/AccountsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UserApp.Service.DTOs.Auths;
+using CareConnect.Service.Services.Users;
+
+namespace CareConnect.WebApi.Controllers;
+
+public class AccountsController(IUserService userService) : ControllerBase
+{
+    [HttpGet("login")]
+    public async Task<IActionResult> LoginAsync(LoginCreateModel createModel)
+    {
+        return Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Data = await userService.LoginAsync(createModel)
+        });
+    }
+}
