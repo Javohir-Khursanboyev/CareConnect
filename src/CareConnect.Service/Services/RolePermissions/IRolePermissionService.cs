78e07aefa2b0a6597d641d1b6c007f9ccc8b2b4c
Komiljon <komiljonrahmatov03@gmail.com>
2024-05-22 00:18:33 +0500

    Added IRolePermissionService

---
+++ src/CareConnect.Service/Services/RolePermissions/IRolePermissionService.cs
@@ -0,0 +1,14 @@
+﻿using CareConnect.Service.Configurations;
+using CareConnect.Service.DTOs.RolePermissions;
+
+namespace CareConnect.Service.Services.RolePermissions;
+
+public interface IRolePermissionService
+{
+    Task<RolePermissionViewModel> CreateAsync(RolePermissionCreateModel model);
+    Task<RolePermissionViewModel> UpdateAsync(long id, RolePermissionUpdateModel model);
+    Task<bool> DeleteAsync(long id);
+    Task<RolePermissionViewModel> GetByIdAsync(long id);
+    Task<IEnumerable<RolePermissionViewModel>> GetAllAsync(PaginationParams @params, Filter filter, string search = null);
+    bool CheckRolePermission(string role, string action, string controller);
+}
