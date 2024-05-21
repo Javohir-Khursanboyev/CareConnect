9fbe990446320205fa09ec55a98f075017799056
Javohir <161417608+Javohir-Khursanboyev@users.noreply.github.com>
2024-05-21 21:21:03 +0500

    Added Auth Helper

---
+++ src/CareConnect.Service/Helpers/AuthHelper.cs
@@ -0,0 +1,31 @@
+﻿using CareConnect.Domain.Entities.Users;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+
+namespace CareConnect.Service.Helpers;
+
+public class AuthHelper
+{
+    public static string GenerateToken(User user)
+    {
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var tokenKey = Encoding.UTF8.GetBytes(EnvironmentHelper.JWTKey);
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(new Claim[]
+            {
+                 new Claim("Id", user.Id.ToString()),
+                 new Claim("Phone", user.Phone),
+                 new Claim(ClaimTypes.Role, user.Role.Name)
+            }),
+            Expires = DateTime.UtcNow.AddHours(Convert.ToInt32(EnvironmentHelper.TokenLifeTimeInHours)),
+            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(tokenKey), SecurityAlgorithms.HmacSha256Signature)
+        };
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+
+        return tokenHandler.WriteToken(token);
+    }
+}
