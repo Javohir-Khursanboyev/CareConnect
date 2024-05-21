bbb1d60951d653582f838ab7d6d9daceb2eba03b
Komiljon <komiljonrahmatov03@gmail.com>
2024-05-22 00:53:20 +0500

    Added IHospitalService

---
+++ src/CareConnect.Service/Services/Hospitals/IHospitalService.cs
@@ -0,0 +1,13 @@
+﻿using CareConnect.Service.Configurations;
+using CareConnect.Service.DTOs.Hospitals;
+
+namespace CareConnect.Service.Services.Hospitals;
+
+public interface IHospitalService
+{
+    Task<HospitalViewModel> CreateAsync(HospitalCreateModel model);
+    Task<HospitalViewModel> UpdateAsync(long id, HospitalUpdateModel model);
+    Task<bool> DeleteAsync(long id);
+    Task<HospitalViewModel> GetByIdAsync(long id);
+    Task<IEnumerable<HospitalViewModel>> GetAllAsync(PaginationParams @params, Filter filter, string search = null);
+}
