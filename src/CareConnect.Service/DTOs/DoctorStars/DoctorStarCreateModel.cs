376df8bd1de23caea0f5d17ddad5a35e5ee521f5
Komiljon <komiljonrahmatov03@gmail.com>
2024-05-21 22:44:33 +0500

    Added DoctorStar DTOs

---
+++ src/CareConnect.Service/DTOs/DoctorStars/DoctorStarCreateModel.cs
@@ -0,0 +1,8 @@
+﻿namespace CareConnect.Service.DTOs.DoctorStars;
+
+public class DoctorStarCreateModel
+{
+    public long DoctorId { get; set; }
+    public long PatientId { get; set; }
+    public byte Star { get; set; }
+}
