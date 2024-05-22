4d22257faa41a429bec2f07611d5c55a070db9a7
Komiljon <komiljonrahmatov03@gmail.com>
2024-05-22 14:40:42 +0500

    Added User Validators

---
+++ src/CareConnect.Service/Validators/Users/UserChangePasswordModelValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using CareConnect.Service.Helpers;
+using CareConnect.Service.DTOs.Users;
+
+namespace CareConnect.Service.Validators.Users;
+
+public class UserChangePasswordModelValidator : AbstractValidator<UserChangePasswordModel>
+{
+    public UserChangePasswordModelValidator()
+    {
+        RuleFor(up => up.OldPassword)
+            .NotNull()
+            .WithMessage(up => $"{nameof(up.OldPassword)} is not specified");
+
+        RuleFor(up => up.NewPassword)
+            .Must(ValidationHelper.IsPasswordHard);
+
+        RuleFor(up => up.ConfirmPassword)
+            .NotNull()
+            .WithMessage(up => $"{nameof(up.OldPassword)} is not specified");
+    }
+}
---
+++ src/CareConnect.Service/Validators/Users/UserCreateModelValidator.cs
@@ -0,0 +1,37 @@
+﻿using FluentValidation;
+using CareConnect.Service.Helpers;
+using CareConnect.Service.DTOs.Users;
+
+namespace CareConnect.Service.Validators.Users;
+
+public class UserCreateModelValidator : AbstractValidator<UserCreateModel>
+{
+    public UserCreateModelValidator()
+    {
+        RuleFor(user => user.FirstName)
+            .NotNull()
+            .WithMessage(user => $"{nameof(user.FirstName)} is not specified");
+
+        RuleFor(user => user.LastName)
+           .NotNull()
+           .WithMessage(user => $"{nameof(user.LastName)} is not specified");
+
+        RuleFor(user => user.Email)
+            .Must(ValidationHelper.IsEmailValid);
+
+        RuleFor(user => user.Password)
+            .Must(ValidationHelper.IsPasswordHard);
+
+        RuleFor(user => user.Phone)
+            .NotNull()
+            .WithMessage(user => $"{nameof(user.Phone)} is not specified");
+
+        RuleFor(user => user.Gender)
+            .NotNull()
+            .WithMessage(user => $"{nameof(user.Gender)} is not specified");
+
+        RuleFor(user => user.DateOfBirth)
+            .NotNull()
+            .WithMessage(user => $"{nameof(user.DateOfBirth)} is not specified");
+    }
+}
