6340744c9e9186989c63403401a5aeb4cdff215d
Komiljon <komiljonrahmatov03@gmail.com>
2024-05-22 10:01:05 +0500

    Added IDoctorService

---
+++ src/CareConnect.Service/Services/DoctorStars/IDoctorStarService.cs
@@ -0,0 +1,12 @@
+﻿using CareConnect.Service.Configurations;
+using CareConnect.Service.DTOs.DoctorStars;
+
+namespace CareConnect.Service.Services.DoctorStars;
+
+public interface IDoctorStarService
+{
+    Task<DoctorStarViewModel> CreateAsync(DoctorStarCreateModel model);
+    Task<bool> DeleteAsync(long id);
+    Task<DoctorStarViewModel> GetByIdAsync(long id);
+    Task<IEnumerable<DoctorStarViewModel>> GetAllAsync(PaginationParams @params, Filter filter, string search = null);
+}
