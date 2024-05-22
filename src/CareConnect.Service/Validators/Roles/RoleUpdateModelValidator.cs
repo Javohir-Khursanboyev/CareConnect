87f0ba4e81ed4673c8c309a477cf5b3639229d34
Komiljon <komiljonrahmatov03@gmail.com>
2024-05-22 14:32:26 +0500

    Added Role Validators

---
+++ src/CareConnect.Service/Validators/Roles/RoleUpdateModelValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using CareConnect.Service.DTOs.Roles;
+
+namespace CareConnect.Service.Validators.Roles;
+
+public class RoleUpdateModelValidator : AbstractValidator<RoleUpdateModel>
+{
+    public RoleUpdateModelValidator()
+    {
+        RuleFor(role => role.Name)
+            .NotNull()
+            .WithMessage(role => $"{nameof(role.Name)} is not specified");
+    }
+}
