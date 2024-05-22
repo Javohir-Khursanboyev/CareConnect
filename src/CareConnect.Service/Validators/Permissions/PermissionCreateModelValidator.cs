c195e2676bd80798b25f0259758e635eb85a22b4
Komiljon <komiljonrahmatov03@gmail.com>
2024-05-22 13:57:36 +0500

    Added Permission Validators

---
+++ src/CareConnect.Service/Validators/Permissions/PermissionCreateModelValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using CareConnect.Service.DTOs.Permissions;
+
+namespace CareConnect.Service.Validators.Permissions;
+
+public class PermissionCreateModelValidator : AbstractValidator<PermissionCreateModel>
+{
+    public PermissionCreateModelValidator()
+    {
+        RuleFor(role => role.Action)
+            .NotNull()
+            .WithMessage(role => $"{nameof(role.Action)} is not specified");
+
+        RuleFor(role => role.Controller)
+           .NotNull()
+           .WithMessage(role => $"{nameof(role.Controller)} is not specified");
+    }
+}
